bf0af3ab7020b5200e5afaaaa94eca9f850090f6
Tornelius Broadwater jr <tkbjr01@icloud.com>
2023-10-29 21:57:31 -0400

    Added Library array, book constructor, and the ability to add created books to books array

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const myLibrary = [];
+
+function Book(author, title, pages, isCompleted) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.isCompleted = isCompleted;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
